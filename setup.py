366269c8c693153a01f42dfd358c08545dac9044
David MacLeod <macleod.david@live.co.uk>
2019-09-16 19:06:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='vsdebug',
+    py_modules=['vsdebug'],
     entry_points={
         'console_scripts': [
             'vsdebug = vsdebug:parse_input',
