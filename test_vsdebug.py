700c37ad075029863695ebb248181fb3d022c20f
David MacLeod <davidma@speechmatics.com>
2019-11-08 21:12:00 +0000

    Improved parsing

---
+++ test_vsdebug.py
@@ -0,0 +1,44 @@
+from vsdebug import parse_parts, split_input
+
+
+def test_python():
+    input_str = "python3 code.py --param1=1  --param2=2 --param3 3"
+
+    #parse_input(input_str)
+    assert True
+
+
+class Test_split_input:
+
+    def test_simple(self):
+        input_str = "--a=1 --b 2"
+        ref = ["--a", "1", "--b", "2"]
+        result = split_input(input_str)
+        assert ref == result
+
+    def test_double(self):
+        input_str = "--a==1  --b 2"
+        ref = ["--a", "1", "--b", "2"]
+        result = split_input(input_str)
+        assert ref == result
+
+    def test_literal(self):
+        input_str = "--a='=' --b 2"
+        ref = ["--a", "'='", "--b", "2"]
+        result = split_input(input_str)
+        assert ref == result
+
+class Test_parse_parts:
+
+    def test_program(self):
+        parts = ["python3", "code.py", "--param1", "1", "--param2", "--param3", "3"]
+        ref = ("program", "code.py", ['"--param1", "1",', '"--param2",', '"--param3", "3",'])
+        result = parse_parts(parts)
+        assert ref == result
+
+
+    def test_module(self):
+        parts = ["python3", "-m", "package.code", "--param1", "1", "--param2", "--param3", "3"]
+        ref = ("module", "package.code", ['"--param1", "1",', '"--param2",', '"--param3", "3",'])
+        result = parse_parts(parts)
+        assert ref == result
--- vsdebug.py
+++ vsdebug.py
@@ -1,28 +1,91 @@
-
-def parse_input(input_str):
+def parse_parts(s):
     exec_type = 'program'
     args = ''
     name = ''
+    args = []
 
-    s = input_str.split(' ')
+    assert s[0].startswith('python'), "input must start with python command"
 
-    for i in range(len(s)):
-        
+    i = 0
+    while i < len(s):
         if s[i].startswith('python'):
             if s[i+1] == '-m':
                 exec_type = 'module'
-                name = s[i+2]
+                name_offset = 2
             else:
-                name = s[i+1]
-                
-        elif s[i].startswith('--'):
-            args += f'\n            "{s[i]}", '
-            
-            if not s[i+1].startswith('--'):
-                args += f'"{s[i+1]}",'
+                name_offset = 1
+            name = s[i+name_offset] 
+            i += name_offset + 1
 
-    if not name:
-        raise ValueError('Input must contain python command')
+        elif s[i].startswith('--'):
+
+            if not s[i+1].startswith('--'):                
+                arg_offset = 1
+                arg = f'"{s[i]}", "{s[i+arg_offset]}",'
+            else:
+                arg_offset = 0
+                arg = f'"{s[i]}",'
+
+            args.append(arg)
+            i += arg_offset + 1
+
+    return exec_type, name, args
+            
+
+
+             
+            # args += f'\n            "{s[i]}", '
+            
+            # if i+1 < len(s) and not s[i+1].startswith('--'):
+            #     args += f'"{s[i+1]}",'
+
+    return name, exec_type, args
+    # if not name:
+    #     raise ValueError('Input must contain python command')
+
+    # launch_profile = f'''
+    # {{
+    #     "name": "Python: Debug profile",
+    #     "type": "python",
+    #     "request": "launch",
+    #     "{exec_type}": "{name}",
+    #     "console": "integratedTerminal",
+    #     "cwd": "${{workspaceFolder}}",
+    #     "justMyCode": false,
+    #     "args": [{args}
+    #     ]
+    # }},
+    # '''
+    # return launch_profile
+
+def split_input(s):
+    # can we use a regex instead? 🤔
+    parts = []
+    quote = ''
+    part = ''
+
+    for i in range(len(s)):
+        if quote:
+            part += s[i]
+            if s[i] == quote:
+                quote = ''
+        elif s[i] in ' =':
+            if part:
+                parts.append(part)
+                part = ''
+        else:
+            part += s[i] 
+            if s[i] in '\'"':
+                quote = s[i]
+
+    if part:
+        parts.append(part)
+
+    return parts
+
+def build_launch_profile(exec_type, name, args):
+
+    args_str = '\n            '.join(args)
 
     launch_profile = f'''
     {{
@@ -33,15 +96,20 @@ def parse_input(input_str):
         "console": "integratedTerminal",
         "cwd": "${{workspaceFolder}}",
         "justMyCode": false,
-        "args": [{args}
+        "args": [{args_str}
         ]
     }},
     '''
     return launch_profile
 
+
+
 def main():
     import sys
-    print(parse_input(sys.argv[1]))
+
+    parts = split_input(sys.argv[1])
+    exec_type, name, args = parse_parts(parts)
+    print(build_launch_profile(exec_type, name, args))
 
 
 if __name__ == '__main__':
